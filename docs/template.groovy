ccc35f7738fcf530edad485581636b9246448f17
Regexp <regexpei@163.com>
2024-05-25 19:08:27 +0800

    更新 Jenkins 流水线模板

--- docs/template.groovy
+++ docs/template.groovy
@@ -1,27 +1,36 @@
 import groovy.json.JsonOutput
 
 pipeline {
-    agent {
-        label 'xxx'
-    }
+    // 代理，即执行脚本的机器
+    agent { label 'xxx' }
+    // 环境变量
     environment {
         key = value
     }
+    // 对应步骤
     stages {
+        // 对应任务
         stage('xxx') {
+            // 若有指定，则在该节点执行脚本
+            agent {label 'xxx'}
             steps {
                 script {
+                    // 对应任务项
                     methodA()
                     methodB()
-                    methodC()
+                }
+            }
+        }
+        stage('yyy') {
+            steps {
+                script {
+                    // 对应任务项
+                    methodA()
+                    methodB()
                 }
             }
         }
     }
 }
-
 void methodA() {}
-
 void methodB() {}
-
-void methodC() {}
